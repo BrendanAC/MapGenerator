f0a64e7fd3dca520b804d5c50a67cc983c560a73
Brendan Currie <wishbone561@gmail.com>
2017-07-17 18:06:32 -0400

    first commit

---
+++ MapObject.py
@@ -0,0 +1,7 @@
+class Map:
+    def __init__(self, x=0,y=0):
+        mapAccpetable=False
+        map[x][y]
+        for i in range(0,x):
+            for j in range(0,y):
+                map[i][j]="0"
---
+++ Ui.py
@@ -0,0 +1,7 @@
+import MapObject as MapO
+def main():
+    print("Welcome to Map Generator v 1.0")
+    Localmap=MapO
+
+if __name__ == "__main__":
+    main()
