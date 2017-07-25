eeccc9586ff5a4f7387c8759d45eb2d99ee589ed
Brendan Currie <wishbone561@gmail.com>
2017-07-25 12:41:22 -0400

    added a couple classes to make object orientated

--- MapObject.py
+++ MapObject.py
@@ -1,7 +1,16 @@
+import random
 class Map:
     def __init__(self, x=0,y=0):
         mapAccpetable=False
-        map[x][y]
+
+        self.map = [[0] * x for i in range(y)]
         for i in range(0,x):
             for j in range(0,y):
-                map[i][j]="0"
+                self.map[i][j]="0"
+
+    def New(self,x,y):
+
+        for i in range(0,x):
+            for j in range(0,y):
+
+                self.map[i][j]=random.randint(0,1)
--- Ui.py
+++ Ui.py
@@ -1,7 +1,11 @@
-import MapObject as MapO
+from MapObject import Map
 def main():
     print("Welcome to Map Generator v 1.0")
-    Localmap=MapO
+    x=10
+    y=10
+    Localmap=Map(x,y)
+    Localmap=Map.New(x, y)
+    print(Localmap)
 
 if __name__ == "__main__":
     main()
